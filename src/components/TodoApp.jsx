48c46b26536ed1ff74e6d40ceb33f352e0e14535
Maxim Murysov <murisov98@mail.ru>
2026-02-11 20:49:08 +0300

    feat: update TodoApp component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,6 +1,36 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
 function TodoApp() {
-  return <div>fff</div>;
+  const [todos, setTodos] = useState([]);
+  const [form, setForm] = useState("");
+  const addTodo = () => {
+    if (form.trim() === "") return;
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text: form,
+      completed: false,
+    };
+    setTodos([newTodo, ...todos]);
+    setForm("");
+  };
+  return (
+    <div className={styles.todoApp}>
+      <div className={styles["todoApp-container"]}>
+        <div className={styles["add-todo"]}>
+          <input
+            type="text"
+            value={form}
+            onChange={(e) => setForm(e.target.value)}
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
+        <div className={styles["todos-card"]}>
+          {todos.map((elem) => (
+            <div key={elem.id}>{elem.text}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default TodoApp;
