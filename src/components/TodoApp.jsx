82a9d615c0f1161a5ed024dbf1d62107da53b96b
Maxim Murysov <murisov98@mail.ru>
2026-02-12 10:22:59 +0300

    feat: add todo editing UI and completed styling

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,15 +3,24 @@ import styles from "./styles.module.css";
 function TodoApp() {
   const [todos, setTodos] = useState([]);
   const [form, setForm] = useState("");
+  //   const [isEdit, setIdit] = useState(false);
   const addTodo = () => {
     if (form.trim() === "") return;
     const newTodo = {
       id: crypto.randomUUID(),
       text: form,
       completed: false,
+      isEdit: false,
     };
     setTodos([newTodo, ...todos]);
     setForm("");
+  };
+  const toogleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
   };
   return (
     <div className={styles.todoApp}>
@@ -30,8 +39,38 @@ function TodoApp() {
         <div className={styles["todos-card"]}>
           {todos.map((elem) => (
             <div key={elem.id} className={styles["todo-elem"]}>
-              <input type="checkbox" />
-              {elem.text}
+              <div className={styles["todo-value"]}>
+                <input
+                  type="checkbox"
+                  value={elem.completed}
+                  onChange={() => toogleTodo(elem.id)}
+                />
+                <p
+                  style={{
+                    textDecoration: elem.completed ? "line-through" : "none",
+                  }}
+                >
+                  {elem.text}
+                </p>
+              </div>
+
+              <div className={styles["button-group"]}>
+                {elem.isEdit ? (
+                  <>
+                    <button className={styles["active-btn"]}>
+                      Подтвердить
+                    </button>
+                    <button className={styles["active-btn"]}>Отменить</button>
+                  </>
+                ) : (
+                  <>
+                    <button className={styles["active-btn"]}>
+                      Редактировать
+                    </button>
+                    <button className={styles["active-btn"]}>Удалить</button>
+                  </>
+                )}
+              </div>
             </div>
           ))}
         </div>
